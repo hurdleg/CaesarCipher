3dd4cb0c4215b28076601febeace58f464226f33
Gerald Hurdle <hurdleg@algonquincollege.com>
2021-05-27 16:59:44 -0400

    Removed unused static (class) vars: MAX and MIN for rotation.

--- app/src/main/java/com/algonquincollege/hurdleg/caesarcipher/Constants.java
+++ app/src/main/java/com/algonquincollege/hurdleg/caesarcipher/Constants.java
@@ -11,8 +11,6 @@ public class Constants {
     public static final String  ABOUT_DIALOG_TAG = "About";
     public static final String  LOG_TAG          = "CaesarCipher";
     public static final int     ROT13            = 13;
-    public static final int     ROTATION_MAX     = 25;
-    public static final int     ROTATION_MIN     = 0;
     public static final String  ROTATIONS        = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static final String  THE_MESSAGE      = "com.algonquincollege.hurdleg.caesarcipher.message";
     public static final String  THE_ROTATION     = "com.algonquincollege.hurdleg.caesarcipher.rotation";
